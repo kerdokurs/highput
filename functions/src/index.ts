d817177e837207f7174b95b74af6b5c2fe640fd7
Kerdo Kurs <kerdokurs@gmail.com>
2021-05-23 16:47:18 +0300

    add firebase integration

---
+++ functions/src/index.ts
@@ -0,0 +1,51 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+
+const firestore = admin.firestore();
+
+export const newUser = functions
+  .region('europe-west1')
+  .auth.user()
+  .onCreate(async (user) => {
+    const { displayName, email, uid } = user;
+
+    await firestore.collection('users').doc(uid).set({
+      uid,
+      email,
+      displayName,
+      createdAt: admin.firestore.FieldValue.serverTimestamp(),
+    });
+
+    const board = firestore
+      .collection('users')
+      .doc(uid)
+      .collection('boards')
+      .doc();
+    await board.set({
+      title: 'Welcome!',
+      description: `Welcome to Highput, ${displayName || email}!`,
+      created_at: admin.firestore.FieldValue.serverTimestamp(),
+    });
+
+    [
+      {
+        content: 'Sign in',
+        isDone: true,
+      },
+      {
+        content: 'Add a new todo',
+        isDone: false,
+      },
+      {
+        content: 'Create a task board',
+        isDone: false,
+      },
+    ].forEach(async (data) => {
+      await board.collection('todos').add({
+        ...data,
+        created_at: admin.firestore.FieldValue.serverTimestamp(),
+      });
+    });
+  });
