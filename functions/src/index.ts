88cb0ac2f1c7b3f35f02b29b65eede4df639162f
Kerdo Kurs <kerdokurs@gmail.com>
2021-05-24 10:08:33 +0300

    add automatic reminder pubsub

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -52,3 +52,66 @@ export const newUser = functions
       });
     });
   });
+
+export const timer = functions
+  .region('europe-west1')
+  .pubsub.schedule('* * * * *')
+  .timeZone('Europe/Tallinn')
+  .onRun(async (context) => {
+    const messaging = admin.messaging();
+
+    const pad = (num: number) => String(num).padStart(2, '0');
+
+    const now = admin.firestore.Timestamp.now();
+    const date = now.toDate();
+
+    const hours = pad(date.getHours() + 3);
+    const minutes = pad(date.getMinutes());
+    const time = `${hours}:${minutes}`;
+
+    functions.logger.debug(`Time ${time}`);
+
+    const jobs: Promise<any>[] = [];
+
+    const usersDoc = await firestore.collection('users').get();
+    usersDoc.docs.forEach(async (doc) => {
+      const data = doc.data();
+      const { displayName, token } = data;
+
+      if (!token) return;
+
+      const reminders = await doc.ref
+        .collection('reminders')
+        .where('time', '==', time)
+        .get();
+
+      reminders.docs.forEach((reminder) => {
+        const reminderData = reminder.data();
+        const { title } = reminderData;
+
+        if (!title) return;
+
+        const job = messaging
+          .sendToDevice(token, {
+            notification: {
+              title: 'Reminder',
+              body: title,
+            },
+          })
+          .then(() =>
+            functions.logger.info('Reminder sent to', displayName || token)
+          )
+          .catch((e) =>
+            functions.logger.error(
+              'Error occurred while sending to',
+              displayName || token,
+              e
+            )
+          );
+
+        jobs.push(job);
+      });
+    });
+
+    await Promise.all(jobs);
+  });
